ec08b47e41b7366c08dcf8e45aec3fbb64c32535
Shah Riya Naeem <118999488+snrredoy@users.noreply.github.com>
2024-07-16 00:19:52 +0600

    Fix search error , change output style and change decrease quantity code

--- lend_book.py
+++ lend_book.py
@@ -7,22 +7,32 @@ def lend_book():
     if results:
         for idx, book in enumerate(results):
             print(
-                f"{idx + 1}. Title: {book.title}, Authors: {', '.join(book.authors)}, ISBN: {book.isbn}"
+                f"{idx + 1}. Title: {book.title} | Authors: {', '.join(book.authors)} | ISBN: {book.isbn} | Year: {book.year} | Price: {book.price}"
             )
 
         try:
             choice = int(input("Enter the number of the book you want to lend: "))
-            if idx + 1 == choice:
-                if results[idx].quantity > 0:
+            if 1 <= choice <= len(results):
+                book_to_lend = results[choice - 1]
+
+                if book_to_lend.quantity > 0:
                     lent_to = input("Enter the name of the person lending to: ")
-                    results[idx].quantity -= 1
-                    lending_record = model.LendingRecord(results[idx], lent_to)
+                    for book in menu.books:
+                        if book.isbn == book_to_lend.isbn:
+                            book.quantity -= 1
+                            break
+                    lending_record = model.LendingRecord(book_to_lend, lent_to)
                     menu.lending_records.append(lending_record)
                     file_operation.save_books()
                     file_operation.save_lending_records()
-                    print("Book lent successfully!")
+                    print("Book lend successfully!")
+                else:
+                    print("Not enough books available to lend.")
+
             else:
-                print("Not enough books available to lend.")
+                print(
+                    "Invalid choice. Please enter a number corresponding to the listed books."
+                )
 
         except ValueError:
             print("Invalid input. Please enter a valid number.")
