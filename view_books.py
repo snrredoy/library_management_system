56a849c23c87fd3d9b75ae6b4b01bb32588304fc
Shah Riya Naeem <118999488+snrredoy@users.noreply.github.com>
2024-07-16 00:25:18 +0600

    Change output style

--- view_books.py
+++ view_books.py
@@ -4,5 +4,5 @@ import menu
 def view_books():
     for book in menu.books:
         print(
-            f"Title: {book.title}, Authors: {', '.join(book.authors)}, ISBN: {book.isbn}, Year: {book.year}, Price: {book.price}, Quantity: {book.quantity}"
+            f"Title: {book.title} | Authors: {', '.join(book.authors)} | ISBN: {book.isbn} | Year: {book.year} | Price: {book.price} | Quantity: {book.quantity}"
         )
